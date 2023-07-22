aa3b78b1a962e09b6d86bf32860a3b4fa9c0807c
matthew0122 <115114071+matthew0122@users.noreply.github.com>
2023-07-22 16:31:08 -0400

    Add some ability to think

--- src/My Bot/MyBot.cs
+++ src/My Bot/MyBot.cs
@@ -6,27 +6,27 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        double maxEval = positionEvaluater(board, moves[0], 0);
+        double maxEval = positionEvaluater(board, moves[0], 0, 0.0, board.IsWhiteToMove);
         int maxEvalIndex = 0;
         for(int i = 1; i < moves.Length; i++){
-            double eval = positionEvaluater(board, moves[i], 0);
+            double eval = positionEvaluater(board, moves[i], 0, 0.0, board.IsWhiteToMove);
             if(eval > maxEval){
                 maxEval = eval;
                 maxEvalIndex = i;
             }
         }
-        Console.WriteLine(maxEval);
+        //Console.WriteLine("Result: " + maxEval);
         return moves[maxEvalIndex];
     }
-    private double positionEvaluater(Board board, Move move, int movesMade){
-        if(movesMade > 2){
-            return -10000;
+    private double positionEvaluater(Board board, Move move, int movesMade, double eval, bool white){
+        if(movesMade > 3){
+            return eval;
         }
         double pos = 0.0;
         board.MakeMove(move);
         Move[] moves = board.GetLegalMoves();
         pos -= Math.Cbrt(moves.Length - 15.0) * board.PlyCount / 3000;
-        if(!board.IsWhiteToMove){
+        if(white){
             pos += materialDifference(board);
         }
         else{
@@ -35,16 +35,17 @@ public class MyBot : IChessBot
         if(move.IsCastles){
             pos += 0.3;
         }
-        /*
-        int index;
+        
+        int index= 0;
         for(int i = 0; i < moves.Length; i++){
-            double eval = positionEvaluater(board, moves[i], movesMade+1);
-            if(eval > pos){
-                pos = eval;
+            double newEval = positionEvaluater(board, moves[i], movesMade+1, pos, white);
+            if(newEval > pos){
+                pos = newEval;
                 index = i;
             }
-        }*/
+        }
         board.UndoMove(move);
+        
         return pos;
     }
     private int materialDifference(Board board){
