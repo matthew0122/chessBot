e974b041d5f51936dde4af02f8d32e77d034b3f5
matthew0122 <115114071+matthew0122@users.noreply.github.com>
2023-07-22 16:11:10 -0400

    fix piece eval for black

--- src/My Bot/MyBot.cs
+++ src/My Bot/MyBot.cs
@@ -6,10 +6,10 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        double maxEval = positionEvaluater(board, moves[0]);
+        double maxEval = positionEvaluater(board, moves[0], 0);
         int maxEvalIndex = 0;
         for(int i = 1; i < moves.Length; i++){
-            double eval = positionEvaluater(board, moves[i]);
+            double eval = positionEvaluater(board, moves[i], 0);
             if(eval > maxEval){
                 maxEval = eval;
                 maxEvalIndex = i;
@@ -18,21 +18,32 @@ public class MyBot : IChessBot
         Console.WriteLine(maxEval);
         return moves[maxEvalIndex];
     }
-    private double positionEvaluater(Board board, Move move){
+    private double positionEvaluater(Board board, Move move, int movesMade){
+        if(movesMade > 2){
+            return -10000;
+        }
         double pos = 0.0;
         board.MakeMove(move);
         Move[] moves = board.GetLegalMoves();
-        //pos -= Math.Cbrt(moves.Length - 15.0) * board.PlyCount / 3000;
+        pos -= Math.Cbrt(moves.Length - 15.0) * board.PlyCount / 3000;
         if(!board.IsWhiteToMove){
             pos += materialDifference(board);
         }
         else{
-            pos += Math.Abs(materialDifference(board));
+            pos -= materialDifference(board);
         }
         if(move.IsCastles){
             pos += 0.3;
         }
-        
+        /*
+        int index;
+        for(int i = 0; i < moves.Length; i++){
+            double eval = positionEvaluater(board, moves[i], movesMade+1);
+            if(eval > pos){
+                pos = eval;
+                index = i;
+            }
+        }*/
         board.UndoMove(move);
         return pos;
     }
