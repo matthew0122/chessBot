84b5a2d223de26e0e77031c91675438a89c8902f
matthew0122 <115114071+matthew0122@users.noreply.github.com>
2023-07-23 17:32:58 -0400

    Bot is actually decent, just slow

--- src/My Bot/MyBot.cs
+++ src/My Bot/MyBot.cs
@@ -10,7 +10,11 @@ public class MyBot : IChessBot
         int maxEvalIndex = 0;
         for(int i = 1; i < moves.Length; i++){
             double eval = moveEvaluater(board, moves[i], 0, 0.0, board.IsWhiteToMove);
-            if(eval > maxEval){
+            if(eval > maxEval && board.IsWhiteToMove){
+                maxEval = eval;
+                maxEvalIndex = i;
+            }
+            else if(eval < maxEval){
                 maxEval = eval;
                 maxEvalIndex = i;
             }
@@ -25,37 +29,51 @@ public class MyBot : IChessBot
         
         board.MakeMove(move);
         double pos = positionEvaluator(board, white);
-        if(move.IsCastles){
+        if(board.PlyCount < 4 && move.MovePieceType == PieceType.Pawn && isMyTurn(board, white)){
+            pos -= 0.3;
+        }
+        else if(board.PlyCount < 10 && (move.MovePieceType == PieceType.King || move.MovePieceType == PieceType.Pawn || move.MovePieceType == PieceType.Queen)){
+            pos += 0.3;
+        }
+        if(move.IsCastles && !isMyTurn(board, white)){
             pos += 1.5;
         }
-        if(move.MovePieceType == PieceType.King && board.PlyCount < 60 && ((!white && board.IsWhiteToMove) || (white && !board.IsWhiteToMove))){
-            pos -= 100.0;
+        /*else if(move.MovePieceType == PieceType.King && board.PlyCount < 80 && !isMyTurn(board, white)){
+            pos += 1000.0;
+        }*/
+        if(move.IsCapture){
+            if(move.CapturePieceType > move.MovePieceType && !isMyTurn(board, white)){
+                pos += move.CapturePieceType - move.MovePieceType;
+            }
         }
         Move[] moves = board.GetLegalMoves();
-        /*if(board.IsInCheckmate() && ((!white && board.IsWhiteToMove) || (white && !board.IsWhiteToMove))){
+        /*if(board.IsInCheckmate() && isMyTurn(board, white)){
             board.UndoMove(move);
             Console.WriteLine(move);
-            return 10000;
+            return 1000000;
         }
         else if(board.IsInCheckmate()){
             board.UndoMove(move);
-            return -10000;
+            return -1000000;
         }*/
         
         
-        int index= 0;
         for(int i = 0; i < moves.Length; i++){
             double newEval = moveEvaluater(board, moves[i], movesMade+1, pos, white);
-            if((!white && board.IsWhiteToMove) || (white && !board.IsWhiteToMove)){
-                if(newEval > pos){
+            if(!isMyTurn(board, white)){
+                if(newEval > pos && white){
                     pos = newEval;
-                    index = i;
+                }
+                else if(newEval < pos && !white){
+                    pos = newEval;
                 }
             }
             else{
-                if(newEval < pos){
+                if(newEval < pos && white){
                     pos = newEval;
-                    index = i;
+                }
+                else if(newEval > pos && !white){
+                    pos = newEval;
                 }
             }
             
@@ -70,13 +88,25 @@ public class MyBot : IChessBot
     }
     private double positionEvaluator(Board board, bool white){
         Move[] moves = board.GetLegalMoves();
-        double pos = Math.Cbrt(moves.Length - 15.0) * board.PlyCount / -3000;
+        double pos;
         if(white){
-            pos += materialDifference(board);
+            pos = Math.Cbrt(moves.Length - 15.0) * board.PlyCount / 3000;
         }
         else{
+            pos = Math.Cbrt(moves.Length - 15.0) * board.PlyCount / -3000;
+        }
+        if(white){
             pos -= materialDifference(board);
         }
+        else{
+            pos += materialDifference(board);
+        }
         return pos;
+    }
+    private bool isMyTurn(Board board, bool white){
+        return ((!white && board.IsWhiteToMove) || (white && !board.IsWhiteToMove));
+    }
+    private int getSpace(Board board){
+        return 0;
     }
 }
